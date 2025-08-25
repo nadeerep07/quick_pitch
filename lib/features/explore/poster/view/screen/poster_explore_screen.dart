f0a95109dadd9e9d84fbf33c018c36a72004e668
nadeer <nadeerep07@gmail.com>
2025-08-25 13:19:00 +0530

    backgorund painter added in explore screen

--- lib/features/explore/poster/view/screen/poster_explore_screen.dart
+++ lib/features/explore/poster/view/screen/poster_explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_pitch_app/core/common/main_background_painter.dart';
 import 'package:quick_pitch_app/core/config/app_colors.dart';
 import 'package:quick_pitch_app/features/explore/poster/repository/poster_explore_repository.dart';
 import 'package:quick_pitch_app/features/explore/poster/service/poster_explore_service.dart';
@@ -33,52 +34,59 @@ class _PosterExploreView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: SafeArea(
-        child: BlocBuilder<PosterExploreCubit, PosterExploreState>(
-          builder: (context, state) {
-            if (state is PosterExploreLoading) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.primary),
-                ),
-              );
-            } else if (state is PosterExploreError) {
-              return ExploreErrorView(
-                message: state.message,
-                onRetry: () => context.read<PosterExploreCubit>().load(),
-              );
-            } else if (state is PosterExploreLoaded) {
-              return Column(
-                children: [
-                  ExploreAppBar(state: state),
-                  if (!state.isMapView) ...[
-                    ExploreSearchBar(
-                      onChanged: (query) =>
-                          context.read<PosterExploreCubit>().updateSearch(query),
+      body: Stack(
+        children: [
+          CustomPaint(
+            size: Size.infinite,
+            painter: MainBackgroundPainter(),),
+          SafeArea(
+            child: BlocBuilder<PosterExploreCubit, PosterExploreState>(
+              builder: (context, state) {
+                if (state is PosterExploreLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(AppColors.primary),
                     ),
-                    ExploreSkillsFilter(
-                      state: state,
-                      onSkillToggle: (skill) =>
-                          context.read<PosterExploreCubit>().toggleSkill(skill),
-                    ),
-                  ],
-                  Expanded(
-                    child: state.isMapView
-                        ? PosterExploreMapView(
-                            fixers: state.fixersWithLocation,
-                            posterLocation: state.posterLocation,
-                          )
-                        : PosterExploreListView(
-                            fixers: state.filteredFixers,
-                            posterLocation: state.posterLocation,
-                          ),
-                  ),
-                ],
-              );
-            }
-            return const SizedBox.shrink();
-          },
-        ),
+                  );
+                } else if (state is PosterExploreError) {
+                  return ExploreErrorView(
+                    message: state.message,
+                    onRetry: () => context.read<PosterExploreCubit>().load(),
+                  );
+                } else if (state is PosterExploreLoaded) {
+                  return Column(
+                    children: [
+                      ExploreAppBar(state: state),
+                      if (!state.isMapView) ...[
+                        ExploreSearchBar(
+                          onChanged: (query) =>
+                              context.read<PosterExploreCubit>().updateSearch(query),
+                        ),
+                        ExploreSkillsFilter(
+                          state: state,
+                          onSkillToggle: (skill) =>
+                              context.read<PosterExploreCubit>().toggleSkill(skill),
+                        ),
+                      ],
+                      Expanded(
+                        child: state.isMapView
+                            ? PosterExploreMapView(
+                                fixers: state.fixersWithLocation,
+                                posterLocation: state.posterLocation,
+                              )
+                            : PosterExploreListView(
+                                fixers: state.filteredFixers,
+                                posterLocation: state.posterLocation,
+                              ),
+                      ),
+                    ],
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
